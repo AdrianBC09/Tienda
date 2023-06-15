6ed5f5ff76e2b6cb78220986cef0bbfb82eabea5
AdrianBC09 <adrianbonillac09@gmail.com>
2023-06-14 21:44:54 -0600

    clase 5 final

--- src/main/java/com/Tienda/domain/Categoria.java
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -1,31 +1,32 @@
 package com.Tienda.domain;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
-import javax.persistence.*;
+
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
-    
+
     //version de serializacion
-    private static final serialversionUID = 1L;
-    
+    private static final long serialversionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
-    private Long idCtaegoria;//se transforma en id_categoria
+    @Column(name = "id_categoria")
+    private Long idCategoria;//se transforma en id_categoria
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
 
     public Categoria() {
     }
-    
-    
-   public Categoria(String descripcion, String rutaImagen, boolean activo){
-       this.descripcion = descripcion;
-       this.rutaImagen = rutaImagen;
-   }
+
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
 }
