2daf2e7cf9d30db0de2129f74fb886ac232ee5e1
AdrianBC09 <adrianbonillac09@gmail.com>
2023-07-11 11:40:58 -0600

    semana 8

--- src/main/java/com/Tienda/domain/Categoria.java
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -2,30 +2,41 @@ package com.Tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
+
+ 
 
 @Data
 @Entity
-@Table(name = "categoria")
-public class Categoria implements Serializable {
+@Table(name="categoria")
+public class Categoria implements Serializable{
 
-    //version de serializacion
+    //Version de serializacion
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
-    private Long idCategoria;//se transforma en id_categoria
+    @Column(name="id_categoria")
+    private Long idCategoria; // Se transforma en Id_categoria
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
 
+    @OneToMany
+    @JoinColumn(name="id_categoria")
+    private List<Producto> productos;
+
+ 
+
     public Categoria() {
     }
+
+ 
 
     public Categoria(String descripcion, String rutaImagen, boolean activo) {
         this.descripcion = descripcion;
         this.rutaImagen = rutaImagen;
         this.activo = activo;
-    }
+    }   
 }
